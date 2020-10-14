79638de18a329049f6711ed910af204c972879f6
Francisco Alvizo <falvizo@bitmovil.mx>
2020-10-14 07:36:10 -0500

    Ejemplos de la función range() empleada en ciclos for

--- forRange.py
+++ forRange.py
@@ -1,5 +1,26 @@
+#Objetos iterables
+cuadernos = ["Matemáticas","Español","Historia","Geografía"]
 
-for i in range (101):
+#Ejemplo de recorrido de un elemento iterable (lista)
+for cuaderno in cuadernos:
+    print (cuaderno)
+
+#Ejemplo de un rango, con límite inferío 0 implícito,
+# límite superior 30 explícito con incremento de 1 en 1 implícito.
+for i in range(31):
     print(i)
 
+#Ejemplo de un rango, con límite inferío 1,
+# límite superior 30 con incremento de 2 en 2.
+for i in range(1,31,2):
+    print(i)
 
+#Ejemplo de un rango, con límite inferío 1,
+# límite superior 30 con incremento de 1 en 1 implícito.
+for i in range(1,31):
+    print(i)
+
+#Ejemplo de un rango, con límite inferío 30,
+# límite superior 1 con decremento de 1 en 1.
+for i in range(30,0,-1):
+    print(i)
